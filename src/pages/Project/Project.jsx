ab67bd3d350facb5f54bc9bdf0b71df74331018e
bai <baiyshdykanov6@gmail.com>
2022-06-24 18:42:46 +0300

    fixed/function

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AddProject from "../../pages/AddProject";
 import axios from "axios";
 
@@ -7,22 +7,26 @@ import user1 from "../../assets/images/user1.avif"
 import user2 from "../../assets/images/user2.avif"
 import user3 from "../../assets/images/user3.avif"
 import Spinner from "../../components/Spinner";
-import {Link,NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import Layout from "../../components/Layout";
+
 const Project = () => {
-    const [project,setProject] = useState([])
-    const [modalActive,setModalActive] = useState(false)
-    const [isLoading,setIsLoading] = useState(true)
-    const [pagination,setPagination] = useState(true)
+    const [project, setProject] = useState([])
+    const [modalActive, setModalActive] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    const [pagination, setPagination] = useState(true)
     useEffect(() => {
         axios("https://62b301d820cad3685c9916ab.mockapi.io/project/")
             .then((res) => {
                 setProject(res.data)
                 setIsLoading(false)
             })
-    },[setIsLoading,setProject])
+    }, [setIsLoading, setProject])
+    const formatDate = (date) => {
+        return date.split("-").reverse().join(".")
+    }
 
-    if(isLoading) {
+    if (isLoading) {
         return <Spinner/>
     }
     return (
@@ -41,32 +45,37 @@ const Project = () => {
             }
             <h1 className="mt-4 font-bold text-2xl text-gray-500 uppercase mb-8">Проекты</h1>
             <div className=" links flex">
-                <Link className={modalActive?"opacity-0": "ml-2 block uppercase spisok"} to="/projects">Список проектов </Link>
-                <Link className={modalActive?"opacity-0":"ml-4 block   uppercase spisok"} to="/projects">Дорожные карты </Link>
+                <Link className={modalActive ? "opacity-0" : "ml-2 block uppercase spisok"} to="/projects">Список
+                    проектов </Link>
+                <Link className={modalActive ? "opacity-0" : "ml-4 block   uppercase spisok"} to="/projects">Дорожные
+                    карты </Link>
             </div>
             <div className="flex justify-between ">
                 <p className="uppercase text-lg mt-5 text-gray-500">Список проектов </p>
                 <div className="project-btn">
-                    <button  className=" inline-flex items-center
+                    <button className=" inline-flex items-center
             justify-center px-4 py-2 border border-transparent
          rounded-md font-semibold capitalize text-white hover:bg-fuchsia-700
      bg-fuchsia-600 focus:outline-none focus:border-red-700 focus:ring
            focus:ring-red-200 disabled:opacity-25 transition blok"
-                             onClick={() => setModalActive(true)}
+                            onClick={() => setModalActive(true)}
                     >Добавить проект
                     </button>
                 </div>
             </div>
             <div className="flex justify-end items-center pagination mb-4">
-                <NavLink to="/projects" onClick={() => setPagination(true)} className="mr-3 cursor-pointer hover:text-purple-800">
+                <NavLink to="/projects" onClick={() => setPagination(true)}
+                         className="mr-3 cursor-pointer hover:text-purple-800">
                     <i className='bx bx-grid-alt text-gray-400 text-3xl hover:text-purple-800'></i></NavLink>
-                <NavLink to="/projects" onClick={() => setPagination(false)} className="cursor-pointer hover:text-purple-800"><i
+                <NavLink to="/projects" onClick={() => setPagination(false)}
+                         className="cursor-pointer hover:text-purple-800"><i
                     className='bx bx-menu text-gray-400 text-4xl hover:text-purple-800'></i></NavLink>
             </div>
-            <div className={pagination?"ml-7 row pl-3 pr-3 ":null}>
+            <div className={pagination ? "ml-7 row pl-3 pr-3 " : null}>
                 {
                     project.map((project) => (
-                        <div key={project.id} className="col-3 mb-3">
+                        <div key={project.id}
+                             className={pagination ? "col-3 mb-3" : " w-full flex items-center justify-between "}>
                             <div
                                 className="
         c-card
@@ -97,9 +106,11 @@ const Project = () => {
                                 <ul className="mb-2 ml-2 text-gray-500">
                                     <li><h2 className="uppercase text-1xl  mb-2 text-black ">{project.title}</h2></li>
                                     <li className=""><i className='bx bxs-calendar text-lg mr-2'></i>
-                                     {`${project.date1.replace(/-/g, ".")} 
-                                  -  ${project.date2?.replace(/-/g, ".")} .гг`}</li>
-                                    <li className="font-size"><i className='bx bxs-user-rectangle text-xl mr-2'></i>{project.director}</li>
+                                        {formatDate(project.date1)} -
+                                        {formatDate(project.date2)}
+                                    </li>
+                                    <li className="font-size"><i
+                                        className='bx bxs-user-rectangle text-xl mr-2'></i>{project.director}</li>
                                     <li><i className='bx bx-ruble text-xl mr-2'></i>{project.admin}</li>
                                     <div>
                                     </div>
@@ -150,7 +161,7 @@ const Project = () => {
                                         </button>
                                     </div>
                                     <div className=" ml-10 flex justify-end">
-                                        <div className={ modalActive? null : "load" }></div>
+                                        <div className={modalActive ? null : "load"}></div>
                                     </div>
                                 </div>
                             </div>
